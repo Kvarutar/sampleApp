765bc9044a2cfbda5b71028c2eda6e23b567d4d6
Kvarutar <v_aleks_s@mail.ru>
2020-11-16 23:20:00 +0300

    Make a basic User model (including secure passwords)

---
+++ test/models/user_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup #при запуске теста создается экземпляр класса
+    @user = User.new(name:"Example User", email:"example@user.com", password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be validate" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "a"*51
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "a"*256 + "@user.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[example@user.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid adress" do 
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (non-blank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should be long" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+end
